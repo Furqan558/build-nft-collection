d5673557dde770ac059a95379b8b2a70dc720f0a
Furqan558 <furqanhameed558@gmail.com>
2023-04-06 17:09:45 +0500

    updated nft contract

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x62358F7522168680e664E90E696C4581406b7070";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL =
+	"https://vercel.com/furqan558/build-nft-collection/F8a9MgFuMgXMeqQVrxDVvhHott5h/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
